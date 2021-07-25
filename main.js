b6eb5514c833a5844afd74e0635f5d959dc06466
Isaac Baccah <isaacbaccah@gmail.com>
2021-07-25 22:45:39 +0000

    changes made

---
+++ main.js
@@ -0,0 +1,12 @@
+// String, Numbers, Boolean, null, undefined
+
+const name = 'John';
+const age = 30;
+
+
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+
+// Template String
+
+console.log('My name is ${age} and I am ${name}');
